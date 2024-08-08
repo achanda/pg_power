0bf50b903a732fb6c0232b94badc3dd3d58760fc
Abhishek Chanda <abhishek.becs@gmail.com>
2024-08-08 02:15:09 +0000

    Fix sql

--- pg_power--0.0.1.sql
+++ pg_power--0.0.1.sql
@@ -1,17 +1,17 @@
-/* First we have to remove them from the extension */
+/* First we have to remove them from the extension
 ALTER EXTENSION pg_power DROP VIEW pg_stat_power;
-ALTER EXTENSION pg_power DROP FUNCTION pg_stat_power();
+ALTER EXTENSION pg_power DROP FUNCTION pg_stat_power(); */
 
 /* Then we can drop them */
-DROP VIEW pg_stat_power;
-DROP FUNCTION pg_stat_power();
+DROP VIEW IF EXISTS pg_stat_power;
+DROP FUNCTION IF EXISTS pg_stat_power();
 
 /* Now redefine */
 CREATE FUNCTION pg_stat_power(
     OUT userid oid,
     OUT dbid oid,
     OUT query text,
-    OUT power int8,
+    OUT power int8
 )
 RETURNS SETOF record
 AS 'MODULE_PATHNAME'
